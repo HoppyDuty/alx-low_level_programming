5d6d57522ddb0d61d1ceb1c82c01adf1a4d381d8
HoppyDuty <135538786+HoppyDuty@users.noreply.github.com>
2023-09-21 18:32:51 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,28 +1,17 @@
-#include "main.h"
-
-/**
- * leet - encodes a string in 1337
- * @s: string to be encoded
- *
- * Return: the resulting string;
- */
-char *leet(char *s)
-{
-	int i, j;
-
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-			}
-		}
-	}
-
-	return (s);
-}
+#ifndef FILE_MAIN 
+ #define FILE_MAIN 
+  
+ int _putchar(char c); 
+ char *_strcat(char *dest, char *src); 
+ char *_strncat(char *dest, char *src, int n); 
+ char *_strncpy(char *dest, char *src, int n); 
+ int _strcmp(char *s1, char *s2); 
+ void reverse_array(int *a, int n); 
+ char *string_toupper(char *); 
+ char *cap_string(char *); 
+ char *leet(char *); 
+ char *rot13(char *); 
+ void print_number(int n); 
+ char *infinite_add(char *n1, char *n2, char *r, int size_r); 
+  
+ #endif
